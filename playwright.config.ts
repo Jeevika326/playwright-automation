7741d98bb864174edc2d1ee9a0699c43be3071b6
Jeevika <103268333+Jeevika326@users.noreply.github.com>
2025-06-15 11:11:48 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,56 +2,42 @@ import { defineConfig, devices } from '@playwright/test';
 
 const LT_USERNAME = process.env.LT_USERNAME;
 const LT_ACCESS_KEY = process.env.LT_ACCESS_KEY;
-const LT_GRID_URL = `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify({
-  browserName: 'Chrome',
-  browserVersion: 'latest',
-  'LT:Options': {
-    platform: 'Windows 10',
-    build: 'Playwright-Gitpod-Build',
-    name: 'Playwright-Gitpod-Test',
-    user: LT_USERNAME,
-    accessKey: LT_ACCESS_KEY,
-    network: true,
-    video: true,
-    console: true,
-    visual: true,
-    project: 'Playwright-Gitpod',
-    selenium_version: '4.0.0',
-    geoLocation: 'US',
-    resolution: '1920x1080',
-    w3c: true
-  }
-}))}`;
 
 export default defineConfig({
+  testDir: './tests',
   retries: 1,
+  workers: 3,
   fullyParallel: true,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
-    headless: true,
+    baseURL: 'https://www.lambdatest.com',
     trace: 'on',
     video: 'on',
     screenshot: 'only-on-failure',
-    connectOptions: {
-      wsEndpoint: LT_GRID_URL,
-    },
   },
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+
   projects: [
     {
-      name: 'Windows 10 - Chromium',
-      use: {
+      name: 'chromium',
+      use: { 
         ...devices['Desktop Chrome'],
         browserName: 'chromium',
-        viewport: { width: 1920, height: 1080 },
       },
     },
     {
-      name: 'macOS Catalina - Firefox',
-      use: {
+      name: 'firefox',
+      use: { 
         ...devices['Desktop Firefox'],
         browserName: 'firefox',
-        viewport: { width: 1920, height: 1080 },
+      },
+    },
+    {
+      name: 'webkit',
+      use: { 
+        ...devices['Desktop Safari'],
+        browserName: 'webkit',
       },
     },
   ],
 });
+
